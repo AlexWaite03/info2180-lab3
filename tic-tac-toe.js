7793af4b220b4d6f4c4f4170a4ce296825814b75
Alexander Waite <a.waite1703@gmail.com>
2025-10-17 12:44:01 -0500

    Exercise 4

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -11,6 +11,8 @@ document.addEventListener("DOMContentLoaded", function () {
         square.textContent = Player;
         // Mark the square with the current player's symbol
         square.classList.add(Player);
+        // Check for a winner after each turn
+        winner();
         // Switch player after each turn
         Player = Player === "X" ? "O" : "X";
       }
@@ -27,4 +29,40 @@ document.addEventListener("DOMContentLoaded", function () {
         square.classList.remove("hover");
     });
   });
+
+  function winner (){
+    // winning combos
+    const winning = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (const combination of winning) {
+    
+      const status = document.getElementById("status");
+      const [a, b, c] = combination;
+      // Checks if X won the game
+      if (squares[a].classList.contains("X") && squares[b].classList.contains("X") && squares[c].classList.contains("X")) {
+        // Declare X as the winner
+        status.textContent = "Congratulations! X is the Winner!";
+        // Add class to style the winning message
+        status.classList.add("you-won");
+        return;
+      }
+      // Check for O won the game
+      if (squares[a].classList.contains("O") && squares[b].classList.contains("O") && squares[c].classList.contains("O")) {
+        // Declare O as the winner
+        status.textContent = "Congratulations! O is the Winner!";
+        // Add class to style the winning message
+        status.classList.add("you-won");
+        return;
+      }
+    }
+  }
 });
