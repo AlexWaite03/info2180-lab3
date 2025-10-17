5df302d8f82ece97fb2e89590792d157425a63a4
Alexander Waite <a.waite1703@gmail.com>
2025-10-17 11:22:40 -0500

    Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   const squares = document.querySelectorAll("#board div");
+  //starting playing letter
+  let Player = "X";
   squares.forEach(function (square) {
     square.classList.add("square");
+    //makes each square clickable so it can be marked with an X or O
+    square.addEventListener("click", function () {
+        // Only allow marking the square if it is not already marked to prevent overriding
+      if (!square.classList.contains("X") && !square.classList.contains("O")) {
+        square.textContent = Player;
+        // Mark the square with the current player's symbol
+        square.classList.add(Player);
+        // Switch player after each turn
+        Player = Player === "X" ? "O" : "X";
+      }
+    });
   });
 });
